1264591403cee610f875d85dee6f76d6fe5c69d7
Spjak231 <147488579+Spjak231@users.noreply.github.com>
2024-07-31 15:24:11 +0530

    import java.util.Scanner; public class Demo{     public static void main(String[] args){         int a, b;         Scanner read = new Scanner(System.in);         a = read.nextInt();         b = read.nextInt();         System.out.println(a%b);     }
    }

---
+++ Modulus.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Demo{
+    public static void main(String[] args){
+        int a, b;
+        Scanner read = new Scanner(System.in);
+        a = read.nextInt();
+        b = read.nextInt();
+        System.out.println(a%b);
+    }
+    
+}
