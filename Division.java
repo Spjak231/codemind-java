c722c19d7b92657566d248420e7eb7e594eb31e9
Spjak231 <147488579+Spjak231@users.noreply.github.com>
2024-07-31 15:32:04 +0530

    import java.util.Scanner; public class demo{     public static void main(String[] args){         int a, b;         Scanner read = new Scanner(System.in);         a = read.nextInt();         b = read.nextInt();         System.out.println(a/b);     } }

---
+++ Division.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class demo{
+    public static void main(String[] args){
+        int a, b;
+        Scanner read = new Scanner(System.in);
+        a = read.nextInt();
+        b = read.nextInt();
+        System.out.println(a/b);
+    }
+}
