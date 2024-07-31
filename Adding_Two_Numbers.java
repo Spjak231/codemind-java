e0c9c9da8e0a9bde468c7b6ba7b35f8b86bd5ee1
Spjak231 <147488579+Spjak231@users.noreply.github.com>
2024-07-31 15:18:03 +0530

    import java.util.Scanner; public class Add{     public static void main(String[] args){             int a,b,c;             Scanner read = new Scanner(System.in);             a = read.nextInt();             b = read.nextInt();             c = a+b;             System.out.println(c);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Add{
+    public static void main(String[] args){
+            int a,b,c;
+            Scanner read = new Scanner(System.in);
+            a = read.nextInt();
+            b = read.nextInt();
+            c = a+b;
+            System.out.println(c);
+    }
+}
